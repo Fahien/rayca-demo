566896ac2a40c273eb470445d083c51bc2a3ff3e
Antonio Caggiano <info@antoniocaggiano.eu>
2025-07-11 16:42:50 +0200

    demo: GLTF

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@ fn main_loop(mut win: Win) {
     let mut timer = Timer::new();
 
     let mut vkr = Vkr::new(&mut win);
-    let (width, height) = (win.size.width, win.size.height);
 
     let main_pipeline = PipelineMain::new::<Vertex>(
         #[cfg(target_os = "android")]
@@ -47,145 +46,22 @@ fn main_loop(mut win: Win) {
         &vkr.pass,
     );
 
-    let mut model = RenderModel::new(&vkr.dev);
+    // Get model path from CLI
+    let model_path = std::env::args()
+        .nth(1)
+        .unwrap_or("models/box-textured/BoxTextured.gltf".to_string());
 
-    let camera = model.push_camera(Camera::orthographic(
-        width as f32 / 480.0,
-        height as f32 / 480.0,
-        0.1,
-        1.0,
-    ));
+    let gltf_model =
+        Model::load_gltf_path(model_path, &vkr.assets).expect("Failed to open gltf model");
+    let mut model = RenderModel::new_with_gltf(&vkr.dev, &vkr.assets, gltf_model);
+
+    let camera = model.push_camera(Camera::infinite_perspective(1.0, 3.14 / 4.0, 0.1));
     let camera_node = Node::builder()
         .camera(camera)
-        .trs(Trs::builder().translation(Vec3::new(0.0, 0.0, 0.0)).build())
+        .trs(Trs::builder().translation(Vec3::new(0.0, 0.0, 4.0)).build())
         .build();
     let camera_node_handle = model.push_node(camera_node);
     model.push_to_scene(camera_node_handle);
-
-    let image = Image::builder().uri("images/test.png").build();
-    let image_handle = model.push_image(image, &vkr.assets);
-    let sampler_handle = model.push_sampler(Sampler::default());
-    let texture_handle = model.push_texture(Texture::new(image_handle, sampler_handle));
-
-    let lines_material = model.push_material(Material::builder().shader(1).build());
-
-    let lines_primitive = {
-        // Notice how the first line appears at the top of the picture as Vulkan Y axis is pointing downwards
-        let vertices = vec![
-            Vertex::builder()
-                .position(Point3::new(-0.5, -0.5, 0.0))
-                .color(Color::new(1.0, 1.0, 0.0, 1.0))
-                .build(),
-            Vertex::builder()
-                .position(Point3::new(0.5, -0.5, 0.0))
-                .color(Color::new(1.0, 1.0, 0.0, 1.0))
-                .build(),
-            Vertex::builder()
-                .position(Point3::new(0.5, 0.5, 0.0))
-                .color(Color::new(1.0, 0.5, 0.0, 1.0))
-                .build(),
-            Vertex::builder()
-                .position(Point3::new(-0.5, 0.5, 0.0))
-                .color(Color::new(1.0, 0.1, 0.0, 1.0))
-                .build(),
-            Vertex::builder()
-                .position(Point3::new(-0.5, -0.5, 0.0))
-                .color(Color::new(1.0, 0.0, 0.3, 1.0))
-                .build(),
-        ];
-        Primitive::builder()
-            .vertices(vertices)
-            .mode(PrimitiveMode::Lines)
-            .material(lines_material)
-            .build()
-    };
-
-    let lines_primitive_handle = model.push_primitive(lines_primitive);
-    let lines_mesh = model.push_mesh(Mesh::builder().primitive(lines_primitive_handle).build());
-    let lines = model.push_node(
-        Node::builder()
-            .trs(Trs::builder().translation(Vec3::new(0.5, 0.5, 0.3)).build())
-            .mesh(lines_mesh)
-            .build(),
-    );
-    model.push_to_scene(lines);
-
-    let rect_material = model.push_material(
-        Material::builder()
-            .albedo(texture_handle.id.into())
-            .build(),
-    );
-
-    let rect_primitive = {
-        let vertices = vec![
-            Vertex::builder()
-                .position(Point3::new(-0.2, -0.2, 0.0))
-                .uv(Vec2::new(0.0, 0.0))
-                .build(),
-            Vertex::builder()
-                .position(Point3::new(0.2, -0.2, 0.0))
-                .uv(Vec2::new(1.0, 0.0))
-                .build(),
-            Vertex::builder()
-                .position(Point3::new(-0.2, 0.2, 0.0))
-                .uv(Vec2::new(0.0, 1.0))
-                .build(),
-            Vertex::builder()
-                .position(Point3::new(0.2, 0.2, 0.0))
-                .uv(Vec2::new(1.0, 1.0))
-                .build(),
-        ];
-        let indices: Vec<u8> = vec![0, 1, 2, 1, 3, 2];
-
-        Primitive::builder()
-            .vertices(vertices)
-            .indices(
-                PrimitiveIndices::builder()
-                    .indices(indices)
-                    .index_type(ComponentType::U8)
-                    .build(),
-            )
-            .material(rect_material)
-            .build()
-    };
-
-    let rect_primitive_handle = model.push_primitive(rect_primitive);
-    let rect_mesh = model.push_mesh(Mesh::builder().primitive(rect_primitive_handle).build());
-
-    let rect = model.push_node(
-        Node::builder()
-            .trs(Trs::builder().translation(Vec3::new(0.0, 0.0, 0.2)).build())
-            .mesh(rect_mesh)
-            .build(),
-    );
-    model.push_to_scene(rect);
-
-    let cyan_material_handle = model.push_material(
-        Material::builder()
-            .color(Color::CYAN)
-            .albedo(texture_handle.id.into())
-            .shader(0)
-            .build(),
-    );
-    let cube_primitive_handle = model.push_primitive(
-        Primitive::builder()
-            .cube()
-            .material(cyan_material_handle)
-            .build(),
-    );
-    let cube_mesh = model.push_mesh(Mesh::builder().primitive(cube_primitive_handle).build());
-
-    let cube = model.push_node(
-        Node::builder()
-            .trs(
-                Trs::builder()
-                    .translation(Vec3::new(-0.5, -0.5, 0.0))
-                    .build(),
-            )
-            .mesh(cube_mesh)
-            .build(),
-    );
-    model.push_to_scene(cube);
 
     let mut current_pipeline = 0;
 
@@ -197,22 +73,25 @@ fn main_loop(mut win: Win) {
 
         let delta = timer.get_delta().as_secs_f32();
 
-        let rot = Quat::axis_angle(Vec3::new(0.0, 0.0, 1.0), delta / 2.0);
-        model.get_node_mut(rect).unwrap().trs.rotate(rot);
-
-        let rot = Quat::axis_angle(Vec3::new(0.0, 0.0, 1.0), -delta / 2.0);
-        model.get_node_mut(lines).unwrap().trs.rotate(rot);
+        // Move camera
+        let camera_node = model.get_node_mut(camera_node_handle).unwrap();
+        let mut camera_z_move = 0.0;
+        if win.input.w.is_down() {
+            camera_z_move -= 1.0 * delta;
+        }
+        if win.input.s.is_down() {
+            camera_z_move += 1.0 * delta;
+        }
+        camera_node
+            .trs
+            .translate(Vec3::new(0.0, 0.0, camera_z_move));
 
         {
             // Update camera
             let camera = model.get_camera_mut(camera).unwrap();
-            *camera = Camera::orthographic(
-                win.size.width as f32 / 480.0,
-                win.size.height as f32 / 480.0,
-                0.1,
-                1.0,
-            );
+            *camera = Camera::orthographic(4.0, 4.0, 0.1, 10.0);
         }
+
         let frame = vkr.next_frame(&win).unwrap();
         let Some(mut frame) = frame else {
             continue;
